1831bbbd9b01a1bca53f84cacacc8760d56f462f
Trung Nguyen <nguyenviettrung1995@gmail.com>
2021-09-26 21:43:44 +0700

    Init ec2 module

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.59.0"
+    }
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "remote"
+  workspace = var.network_workspace
+}
+
+module "ec2" {
+  source = "./ec2"
+
+  environment = var.environment
+  deployer_key_name = var.deployer_key_name
+  public_subnet_id = data.terraform_remote_state.network.outputs.public_subnet_id
+  security_groups = [
+    data.terraform_remote_state.network.outputs.default_security_group_id,
+    data.terraform_remote_state.network.outputs.www_security_group_id,
+    data.terraform_remote_state.network.outputs.private_access_security_group_id
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  description = "Environment Name"
+}
+
+variable "network_workspace" {
+  description = "Network workspace name on Terraform Cloud"
+}
+
+variable "deployer_key_name" {
+  description = "Deployer Key Name"
+}
